f4d316841373ccc1b63773dc22783fb181981344
ghalrym <andrewreed2015@gmail.com>
2024-08-09 21:25:43 -0700

    feat: action turtle for parallel processes

--- src/bale_of_turtles/__init__.py
+++ src/bale_of_turtles/__init__.py
@@ -1,3 +1,4 @@
+from .action_turtle import ActionTurtle
 from .agent_turtle import AgentTurtle
 from .llm_turtle import LlmTurtle
 from .tool_turtle import TurtleTool, UnregisteredTurtleEquipment
---
+++ src/bale_of_turtles/action_turtle.py
@@ -0,0 +1,24 @@
+from threading import Thread
+
+from pydantic import BaseModel
+
+from ._turtle_state import _TurtleStateManager
+from .tool_turtle import TurtleTool
+
+
+class ActionTurtle(TurtleTool):
+    __slots__ = ('_thread',)
+
+    def __init__(
+        self,
+        grant_access: bool = False,
+        expect_context: BaseModel | None = None,
+    ):
+        super().__init__(grant_access, expect_context)
+        self._thread: Thread | None = None
+
+    def register(self, state: _TurtleStateManager):
+        self._thread = Thread(target=self.invoke)
+
+    def invoke(self):
+        ...
