1a1905db6aaf9bc822ccef2fd97e9ee6ca6f6b17
ghalrym <andrewreed2015@gmail.com>
2024-08-12 20:23:17 -0700

    remove unused import

--- src/bale_of_turtles/__init__.py
+++ src/bale_of_turtles/__init__.py
@@ -1,6 +1,5 @@
 from .action_turtle import ActionTurtle
 from .agent_turtle import AgentTurtle
-from .llm_turtle import LlmTurtle
 from .tool_turtle import TurtleTool, UnregisteredTurtleEquipment
 from ._turtle_state import use_state, use_trigger
 
